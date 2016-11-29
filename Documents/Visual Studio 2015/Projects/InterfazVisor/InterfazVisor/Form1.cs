71ee036ea75b0fa5287417d7df521894e5c24b48
peter <peterdelgad@gmail.com>
2016-11-29 14:20:41 +0100

    new

---
+++ Documents/Visual Studio 2015/Projects/InterfazVisor/InterfazVisor/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Visor;
+using System.Windows.Forms;
+
+
+
+namespace InterfazVisor
+
+    
+
+{
+    public partial class Form1 : Form
+    {
+        Boolean arrastrando;
+        VisorFoto A = new VisorFoto();
+        Point offset;
+        public Form1()
+
+        {
+            InitializeComponent();
+            arrastrando = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+         private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (arrastrando)
+            {
+                int x = pictureBox1.Location.X + e.X - offset.X;
+                int y = pictureBox1.Location.Y + e.Y - offset.Y;
+                pictureBox1.Location = new Point(x, y);
+
+            }
+
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+
+            arrastrando = true;
+            offset = e.Location;
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            arrastrando = true;
+            offset = e.Location;
+        }
+    }
+}
