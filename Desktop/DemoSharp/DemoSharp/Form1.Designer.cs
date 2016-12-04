626fa59a67fb6e4833bdb9b4a6da870869426d29
peter <peterdelgad@gmail.com>
2016-12-04 18:35:10 +0100

    new

---
+++ Desktop/DemoSharp/DemoSharp/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿namespace InterfazVisor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Abrir = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.Siguiente = new System.Windows.Forms.Button();
+            this.Anterior = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Abrir
+            // 
+            this.Abrir.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Abrir.Location = new System.Drawing.Point(58, 311);
+            this.Abrir.Name = "Abrir";
+            this.Abrir.Size = new System.Drawing.Size(75, 23);
+            this.Abrir.TabIndex = 1;
+            this.Abrir.Text = "Abrir";
+            this.Abrir.UseVisualStyleBackColor = true;
+            this.Abrir.Click += new System.EventHandler(this.button1_Click);
+            this.Abrir.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            this.Abrir.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // Siguiente
+            // 
+            this.Siguiente.Location = new System.Drawing.Point(168, 311);
+            this.Siguiente.Name = "Siguiente";
+            this.Siguiente.Size = new System.Drawing.Size(75, 23);
+            this.Siguiente.TabIndex = 2;
+            this.Siguiente.Text = "Siguiente";
+            this.Siguiente.UseVisualStyleBackColor = true;
+            this.Siguiente.Click += new System.EventHandler(this.button2_Click);
+            this.Siguiente.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            this.Siguiente.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            // 
+            // Anterior
+            // 
+            this.Anterior.Location = new System.Drawing.Point(274, 311);
+            this.Anterior.Name = "Anterior";
+            this.Anterior.Size = new System.Drawing.Size(75, 23);
+            this.Anterior.TabIndex = 3;
+            this.Anterior.Text = "Anterior";
+            this.Anterior.UseVisualStyleBackColor = true;
+            this.Anterior.Click += new System.EventHandler(this.button3_Click);
+            this.Anterior.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            this.Anterior.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(58, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(521, 260);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 369);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Anterior);
+            this.Controls.Add(this.Siguiente);
+            this.Controls.Add(this.Abrir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Abrir;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button Siguiente;
+        private System.Windows.Forms.Button Anterior;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ Desktop/DemoSharp/DemoSharp/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Visor;
+namespace InterfazVisor
+{
+    public partial class Form1 : Form
+    {
+        Boolean arrastrando;
+        Visor.VisorFoto v = new VisorFoto(400, 300);
+        Point offset;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            foreach  (String nom in openFileDialog1.FileNames) {
+                v.Agregar_Foto((Bitmap)Bitmap.FromFile(nom));
+            }
+            pictureBox1.Image = v.Foto_Actual();
+            this.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            v.Resize(50, 50);
+            int ancho = pictureBox1.Width;
+            v.Resize(ancho, 300);
+            pictureBox1.Image = v.Next_Foto();
+            
+
+
+            this.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = v.Prev_Foto();
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+                pasa_foto();
+            if (e.KeyCode == Keys.Left)
+                pictureBox1.Image = v.Prev_Foto();
+        }
+
+        private void pasa_foto()
+        {
+                pictureBox1.Image = v.Next_Foto();
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode != Keys.Tab )
+                    e.IsInputKey = true;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (arrastrando)
+            {
+                int x = pictureBox1.Location.X + e.X - offset.X;
+                int y = pictureBox1.Location.Y + e.Y - offset.Y;
+                pictureBox1.Location = new Point(x, y);
+
+            }
+        }
+    }
+}
