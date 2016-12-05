0876032ccc801b6e7b5db1431e0a1dcf100b5660
peter <peterdelgad@gmail.com>
2016-12-05 12:52:10 +0100

    n

---
+++ Desktop/IntefazVisorPedro/InterfazVisorPedro/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace InterfazVisor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Abrir = new System.Windows.Forms.Button();
+            this.Siguiente = new System.Windows.Forms.Button();
+            this.Anterior = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Abrir
+            // 
+            this.Abrir.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Abrir.Location = new System.Drawing.Point(2, 311);
+            this.Abrir.Name = "Abrir";
+            this.Abrir.Size = new System.Drawing.Size(75, 23);
+            this.Abrir.TabIndex = 1;
+            this.Abrir.Text = "Abrir";
+            this.Abrir.UseVisualStyleBackColor = true;
+            this.Abrir.Click += new System.EventHandler(this.button1_Click);
+            this.Abrir.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            this.Abrir.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            // 
+            // Siguiente
+            // 
+            this.Siguiente.Location = new System.Drawing.Point(83, 311);
+            this.Siguiente.Name = "Siguiente";
+            this.Siguiente.Size = new System.Drawing.Size(75, 23);
+            this.Siguiente.TabIndex = 2;
+            this.Siguiente.Text = "Siguiente";
+            this.Siguiente.UseVisualStyleBackColor = true;
+            this.Siguiente.Click += new System.EventHandler(this.button2_Click);
+            this.Siguiente.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            this.Siguiente.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            // 
+            // Anterior
+            // 
+            this.Anterior.Location = new System.Drawing.Point(83, 340);
+            this.Anterior.Name = "Anterior";
+            this.Anterior.Size = new System.Drawing.Size(75, 23);
+            this.Anterior.TabIndex = 3;
+            this.Anterior.Text = "Anterior";
+            this.Anterior.UseVisualStyleBackColor = true;
+            this.Anterior.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button1_KeyDown);
+            this.Anterior.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(46, 29);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(533, 260);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(195, 312);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Rotar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Rotar_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(352, 311);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Zoom";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Zoom_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(533, 311);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Desplazar";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Desplazar_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(352, 338);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 19);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Zoom out";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Zoom_out_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(195, 341);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(83, 21);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "RotarA";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.RotarAntiHora_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 369);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.Anterior);
+            this.Controls.Add(this.Siguiente);
+            this.Controls.Add(this.Abrir);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button Abrir;
+        private System.Windows.Forms.Button Siguiente;
+        private System.Windows.Forms.Button Anterior;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Desktop/IntefazVisorPedro/InterfazVisorPedro/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Visor;
+namespace InterfazVisor
+{
+    public partial class Form1 : Form
+    {
+        Boolean arrastrando;
+        Visor.VisorFoto v = new VisorFoto(400, 300);
+        Point offset;
+        public Form1()
+        {
+            InitializeComponent();
+            arrastrando = false;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            foreach  (String nom in openFileDialog1.FileNames) {
+                v.Agregar_Foto((Bitmap)Bitmap.FromFile(nom));
+            }
+            pictureBox1.Image = v.Foto_Actual();
+            this.Focus();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            v.Resize(50, 50);
+            int ancho = pictureBox1.Width;
+            v.Resize(ancho, 300);
+            pictureBox1.Image = v.Next_Foto();
+            
+
+
+            this.Focus();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = v.Prev_Foto();
+        }
+
+        private void button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Right)
+                pasa_foto();
+            if (e.KeyCode == Keys.Left)
+                pictureBox1.Image = v.Prev_Foto();
+        }
+
+        private void pasa_foto()
+        {
+                pictureBox1.Image = v.Next_Foto();
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            if (e.KeyCode != Keys.Tab )
+                    e.IsInputKey = true;
+        }
+
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            if (arrastrando)
+            {
+                int x = pictureBox1.Location.X + e.X - offset.X;
+                int y = pictureBox1.Location.Y + e.Y - offset.Y;
+                pictureBox1.Location = new Point(x, y);
+
+            }
+            
+
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            arrastrando = false;
+            offset = e.Location;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            arrastrando = true;
+            offset = e.Location;
+        }
+
+        private void pictureBox1_DoubleClick(object sender, EventArgs e)
+        {
+            arrastrando = false;
+        }
+
+        private void Rotar_Click_1(object sender, EventArgs e)
+        {
+            pictureBox1.Image = v.rotar_hor();
+        }
+
+        private void Zoom_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = v.Zoom(1.1f);
+        }
+
+        private void Desplazar_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = v.Despl(10, 0);
+        }
+
+        private void Zoom_out_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = v.Zoom(-1.1f);
+        }
+
+        private void RotarAntiHora_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = v.rotar_antihor();
+        }
+        
+        
+    }
+}
